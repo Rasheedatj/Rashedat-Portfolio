372260606ce54d5326204408fdaf533492810e47
Rashedat <jinaduikeoluwa@gmail.com>
2026-01-08 15:20:35 +0100

    chore: update conatct icons

--- app/_components/navigation/contacts.tsx
+++ app/_components/navigation/contacts.tsx
@@ -1,34 +1,38 @@
-import Image from 'next/image';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+import { IoIosCall } from 'react-icons/io';
+import { MdOutlineEmail } from 'react-icons/md';
 
 const contacts = [
   {
     id: 'c1',
     title: 'Github',
-    icon: '/icons/github.svg',
+
+    icon: <FaGithub size={24} className='contact' />,
     link: 'https://github.com/Rasheedatj',
   },
   {
     id: 'c2',
     title: 'LinkedIn',
-    icon: '/icons/linkedin.svg',
+    icon: <FaLinkedinIn size={24} className='contact' />,
     link: 'https://www.linkedin.com/in/rashedat-jinadu',
   },
   {
     id: 'c3',
     title: 'X',
-    icon: '/icons/linkedin.svg',
+    icon: <FaXTwitter size={24} className='contact' />,
     link: 'https://twitter.com/Rasheedatj',
   },
   {
     id: 'c4',
     title: 'Email',
-    icon: '/icons/linkedin.svg',
+    icon: <MdOutlineEmail size={24} className='contact' />,
     link: 'https://mail.google.com/jinaduikeoluwa@gmail.com',
   },
   {
     id: 'c5',
     title: 'Calendly',
-    icon: '/icons/linkedin.svg',
+    icon: <IoIosCall size={24} className='contact' />,
     link: 'https://calendly.com/jinaduikeoluwa/let-s-talk-about-your-application',
   },
 ];
@@ -42,18 +46,13 @@ const Contacts = () => {
             key={contact.id}
             href={contact.link}
             target='_blank'
-            className='block relative group hover:-translate-y-2 duration-300 ease-in-out transition-all'
+            className='block relative group'
           >
-            <span className='bg-slate-500 text-white  px-1 py-1 rounded-sm text-xs hidden group-hover:block absolute -top-full left-1/2'>
+            <span className='bg-accent text-slate-800  px-1 py-1 rounded-sm text-xs hidden group-hover:block absolute -top-8 left-1/2'>
               {contact.title}
             </span>
-            <Image
-              src={contact.icon}
-              alt={contact.title}
-              width={24}
-              height={24}
-              className='mt-1'
-            />
+
+            {contact.icon}
           </a>
         );
       })}
